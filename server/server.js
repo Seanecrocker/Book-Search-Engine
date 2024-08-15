6743351f9670b8c06efc789b01cfb222926c240a
Sean <Seanecrocker@gmail.com>
2024-08-15 17:45:00 -0500

    updating server

--- server/server.js
+++ server/server.js
@@ -26,7 +26,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, '../client/dist')));
 }
 
 // Import and use the routes
