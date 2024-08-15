2ab0950f1eb2053366ce3f59031f4224ad7edae9
Sean <Seanecrocker@gmail.com>
2024-08-15 17:21:36 -0500

    updating server

--- server/server.js
+++ server/server.js
@@ -14,14 +14,13 @@ const PORT = process.env.PORT || 3001;
 
 // MongoDB connection using Mongoose
 const connectionString = process.env.MONGODB_URI || 'mongodb://localhost:27017/your-database-name';
-mongoose.connect(connectionString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log('Connected to MongoDB');
-}).catch((err) => {
-  console.error('Error connecting to MongoDB:', err.message);
-});
+mongoose.connect(connectionString)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((err) => {
+    console.error('Error connecting to MongoDB:', err.message);
+  });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
